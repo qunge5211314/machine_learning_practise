823824bc70b9c96cf2ee81c279cd7007a7a74176
韩群 <>
2021-02-01 07:59:27 +0800

    no message

--- algorithm/linear_regression/gradient_descent.py
+++ algorithm/linear_regression/gradient_descent.py
@@ -25,15 +25,18 @@ def boston_housing_price_predict():
     # (4)预估器
     estimator = SGDRegressor(learning_rate="invscaling", eta0=0.03, alpha=0.01)
     estimator.fit(x_train, y_train)
-    joblib.dump(estimator, "../model_save_load/heihei.m")
+    # joblib.dump(estimator, "../model_save_load/heihei.m")
     # (5)得出模型
     print("权重系数为:\n", estimator.coef_)
     print("偏置值为:\n", estimator.intercept_)
     # (6)评估模型
-    y_predict = estimator.predict(x_test)
-    print("预测的房价:\n", y_predict)
-    error = mean_squared_error(y_test, y_predict)
-    print("梯度下降-均方误差为:\n", error)
+    y_test_predict = estimator.predict(x_test)
+    print("预测的房价:\n", y_test_predict)
+    test_error = mean_squared_error(y_test, y_test_predict)
+    print("梯度下降-测试集均方误差为:\n", test_error)
+    y_train_predict = estimator.predict(x_train)
+    train_error = mean_squared_error(y_train, y_train_predict)
+    print("梯度下降-训练集均方误差为:\n", train_error)
 
 
 if __name__ == '__main__':
