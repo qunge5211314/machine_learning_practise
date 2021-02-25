82d937d00ba2b1495c14cad80e71883979a2c82e
JustinHan <>
2021-02-25 14:01:27 +0800

    no message

---
+++ matplotlib_practise/intro.py
@@ -0,0 +1,11 @@
+#!python3
+# -*- coding: utf-8 -*-
+# Author: JustinHan
+# Date: 2021-02-24
+# Introduce: matplotlib介绍
+# Dependence
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [2, 3, 1, 7], "o--r")
+plt.plot([1, 2, 3, 4], [3, 5, 6, 9], "o--b")
+plt.show()
--- matplotlib_practise/practise1.py
+++
@@ -1,4 +0,0 @@
-from matplotlib import pyplot as plt
-
-plt.plot([0, 1, 2, 4, 8, 16, 32, 64])
-plt.show()
